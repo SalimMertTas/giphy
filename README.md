482a234011a55c44efd8f24fe83c779196015c00
SalimMertTas <tassalim000@gmail.com>
2023-02-26 23:24:19 +0300

    add readme.md and update function

---
+++ README.md
@@ -0,0 +1,28 @@
+# GIPHY RED - GIF Search App  
+
+App for searching gifs from GIPHY public API.
+
+Techs:
+- Vanilla JS
+- **GIPHY** public API for database 
+
+Demo:[happy-gifs-demo](https://giphy-red.vercel.app/)
+
+![how-to-use](https://media.giphy.com/media/M9WwBPTYW5eCAsGYSL/giphy.gif)
+
+---
+
+## Installation
+
+### Install Dependencies
+
+```
+npm install
+```
+
+### Run the project
+
+```
+npm run dev
+```
+
--- main.js
+++ main.js
@@ -30,7 +30,7 @@ const renderButton = () =>{
 }
 
 const getGiphy = async (filterValue='all') =>{
-const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=nR0iGdoCbJxTNF9hsVfm5SgF4LTqG0C4&q=${filterValue}&limit=10&offset=0&rating=g&lang=en
+const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=nR0iGdoCbJxTNF9hsVfm5SgF4LTqG0C4&q=${filterValue}&limit=20&offset=0&rating=g&lang=en
 `)
 const giphy = await response.json();
 giphys = giphy.data;
